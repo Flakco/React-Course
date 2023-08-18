3d6fdcaf86f7a92e9b854f93f177cb17a6c7ebec
Flakco <flacobauhaus@gmail.com>
2023-08-18 17:47:30 -0300

    React Course chapter 14 CRUD Operations

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import AddItem from "./AddItem";
 import SearchItem from "./SearchItem";
 import Square from "./Square";
 import Input from "./Input";
+import apiRequest from "./apiRequest";
 
 function App() {
   const API_URL = "http://localhost:3500/items"
@@ -47,17 +48,41 @@ function App() {
     localStorage.setItem("shoppinglist", JSON.stringify(newItems))
   }
 
-  const addItem = (item) => {
+  const addItem = async (item) => {
     const id=items.length ? items[items.length - 1].id + 1 : 1
     const myNewItem={id, checked:false, item}
     const listItems=[...items, myNewItem]
     setAndSaveItems(listItems)
+
+    const postOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(myNewItem)
+    }
+
+    const result = await apiRequest(API_URL,postOptions)
+    if (result) setFetchError(result)
   }
 
-  const handleCheck = (id) => {
+  const handleCheck = async (id) => {
     console.log(`key: ${id}`)
     const listItems = items.map((item) => item.id === id ? {...item, checked: !item.checked} : item)
     setAndSaveItems(listItems)
+
+    const myItem = listItems.filter((item) => item.id === id)
+    const updateOptions = {
+      method: "PATCH",
+      headers: {
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify({checked: myItem[0].checked})
+    }
+    const reqUrl = `${API_URL}/${id}`
+    const result = await apiRequest(reqUrl, updateOptions)
+    if (result) setFetchError(result)
+    
   }
 
   const handleDelete = (id) => {
